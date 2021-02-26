f86edbc41b77f3cf2220c800488cc553abfd1314
David <degasf_1996@hotmail.com>
2021-02-26 08:58:36 -0500

    cargar

---
+++ Cargar a ramas/qr_flutter/lib/src/validacionesRegistro.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/src/registro_usuarios.dart';
+
+class Validaciones {
+  String validateEmail(String value) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return "El correo es necesario";
+    } else if (!regExp.hasMatch(value)) {
+      return "Correo invalido";
+    } else {
+      return null;
+    }
+  }
+
+  //TextEditingController parametro = new TextEditingController();
+  String validatePassword(String value, String parametro) {
+    print("Repas" + value);
+    print("Contra" + parametro);
+    if (value != parametro) {
+      print("Son diferentes");
+      return "Las contraseñas no coinciden";
+    } else {
+      print("Son iguales");
+      print("Repas" + value);
+      print("Contra" + parametro);
+      return null;
+    }
+  }
+
+  String validateName(String value) {
+    String pattern = r'(^[a-zA-Z ]*$)';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return "El nombre es necesario";
+    } else if (!regExp.hasMatch(value)) {
+      return "El nombre debe de ser a-z y A-Z";
+    }
+    return null;
+  }
+
+  String validateMobile(String value) {
+    String patttern = r'(^[0-9]*$)';
+    RegExp regExp = new RegExp(patttern);
+    if (value.length == 0) {
+      return "El telefono es necesariod";
+    } else if (value.length != 10) {
+      return "El numero debe tener 10 digitos";
+    }
+    return null;
+  }
+}
